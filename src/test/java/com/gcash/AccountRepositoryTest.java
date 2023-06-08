5ac20c93c31d6be0bfe0cc5e81b2c4368928d5ce
Mark Awili <markandrew.awili@gmail.com>
2023-06-08 11:09:04 +0800

    Recreate test file for AccountRepositoryTest

--- src/test/java/com/gcash/AccountRepositoryTest.java
+++ src/test/java/com/gcash/AccountRepositoryTest.java
@@ -1,11 +1,24 @@
 package com.gcash;
 
-
 import org.junit.jupiter.api.Test;
 
-public class AccountRepositoryTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountRepositoryTest {
 
     @Test
     void createAccount() {
+    }
+
+    @Test
+    void getAccount() {
+    }
+
+    @Test
+    void deleteAccount() {
+    }
+
+    @Test
+    void getNumberOfAccounts() {
     }
 }
